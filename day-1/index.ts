f03acb1557c95c1a32f5ab18dbc954f63aebee20
adam <adam@harpur.io>
2021-12-01 10:29:28 +0000

    day 1

--- day-1/index.ts
+++ day-1/index.ts
@@ -1,31 +1,30 @@
+const { readFile } = require("fs")
+readFile("input", `utf8`, (err, data) => {
+  if (err) throw err
+  const input = data.split("\n")
 
-    const { readFile } = require("fs")
-    readFile("input", `utf8`, (err, data) => {
-      if (err) throw err
-      const input = data.split("\n")
+  part1(input)
+  part2(input)
+})
 
-      part1(input)
-      part2(input)
-    })
-    
-    function part1(input){
-      let last = input[0]
-      const result = input.reduce((acc, curr) => {
-          acc = last < curr ? acc + 1 : acc
-          last = curr
-          return acc
-        }, 0)
-      console.log(result)
-    }
+function part1(input) {
+  let last = input[0]
+  const result = input.reduce((acc, curr) => {
+    acc = last < curr ? acc + 1 : acc
+    last = curr
+    return acc
+  }, 0)
+  console.log(result)
+}
 
-    function part2(input){
-        let result = []
-        for (let i = 0; i < input.length; i++) {
-          const max = Math.min(input.length, i + 3)
-          const threeOF = input.slice(i, max)
-          const sum = threeOF.reduce((acc, curr) => acc + Number(curr), 0)
-          // @ts-ignore
-          result.push(sum)
-        }
-        part1(result)
-    }
+function part2(input) {
+  let result = []
+  for (let i = 0; i < input.length; i++) {
+    const max = Math.min(input.length, i + 3)
+    const threeOF = input.slice(i, max)
+    const sum = threeOF.reduce((acc, curr) => acc + Number(curr), 0)
+    // @ts-ignore
+    result.push(sum)
+  }
+  part1(result)
+}
