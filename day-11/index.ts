39236d71ffe65a7a1ae1a3d907fb5402ff8cf56b
adam <adam@harpur.io>
2021-12-11 17:08:57 +0000

    day 11

---
+++ day-11/index.ts
@@ -0,0 +1,57 @@
+const input = Deno.readTextFileSync("input")
+  .split("\n")
+  .map((line: any) => line.trim())
+  .map((line: any) => line.split(""))
+
+const grid = input.map((a: Array<string>) => {
+  return a.map((s: string) => Number(s))
+})
+
+function createOctopusMap(grid: any) {
+  const octopi: Octopus[] = []
+  for (const [ri, row] of grid.entries()) {
+    row.forEach((v: number, ci: number) => {
+      octopi.push(createOctopus(ri, ci, v))
+    })
+  }
+  return octopi
+}
+
+function solve(input: any) {
+  const octopi = createOctopusMap(input)
+  for (const octopus of octopi) {
+    console.log(octopus)
+    const visited: Array<Octopus> = []
+    const toVisit: Array<Octopus> = []
+    const { row, col } = octopus
+    if (row - 1 >= 0) {
+      // down
+    }
+    if (row + 1 < octopi.length) {
+      // up
+    }
+    if (col - 1 >= 0) {
+      // left
+    }
+    if (col + 1 < octopi[octopi.length - 1].col) {
+      // right
+    }
+  }
+}
+type Octopus = {
+  id: string
+  energy: number
+  row: number
+  col: number
+  hasFlashed: boolean
+}
+function createOctopus(row: number, col: number, energy: number = 0) {
+  return {
+    id: JSON.stringify({ row: row, col: col }),
+    energy,
+    row,
+    col,
+    hasFlashed: false,
+  }
+}
+solve(grid)
