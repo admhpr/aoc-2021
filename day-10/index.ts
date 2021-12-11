89a683f5932e503af1c0903a0b1c932daef931e8
adam <adam@harpur.io>
2021-12-11 14:40:19 +0000

    day 10

--- day-10/index.ts
+++ day-10/index.ts
@@ -2,10 +2,41 @@
     const input = Deno.readTextFileSync("input").split("\n")
 
     function solve(input: any){
-      console.log(input)
       const openers = [`(`,`[`,`{`,`<`]
-      const closers = [`)`,`]`,`}`,`>`]
-
+      let score = 0;
+      const stack = [];
+      for(const line of input.reverse()){
+        const tokens = line.split("")
+        for(const token of tokens){
+          if(openers.includes(token)){
+            stack.push(token)
+            continue
+          }
+          switch(token){
+            case `)`:
+              if(stack.pop() !== `(`){
+                score += 3
+              }
+              break
+            case `]`:
+              if(stack.pop() !== `[`){
+                score += 57
+              }
+              break
+            case `}`:
+              if(stack.pop() !== `{`){
+                score += 1197
+              }
+              break
+            case `>`:
+              if(stack.pop() !== `<`){
+                score += 25137
+              }
+              break
+          }
+        }
+      }
+      console.log(score)
     }
     solve(input)
   
