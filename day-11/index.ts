39360ecab5505b5160cb3011551b78c538c89805
adam <adam@harpur.io>
2021-12-12 17:13:28 +0000

    day 11

--- day-11/index.ts
+++ day-11/index.ts
@@ -7,103 +7,120 @@ const grid = input.map((a: Array<string>) => {
   return a.map((s: string) => Number(s))
 })
 
-function createOctopusMap(grid: number[][]) {
-  const octopi: Octopus[] = []
-  for (const [ri, row] of grid.entries()) {
-    row.forEach((v: number, ci: number) => {
-      octopi.push(createOctopus(ri, ci, v))
-    })
-  }
-  return octopi
-}
-
-function findOctopusById(id: string, octopi: Octopus[]){
-  const found = octopi.find(o => o.id === id)
-  return found 
-}
-
-function addAllAdjacent(octopus: Octopus, octopi: Octopus[]){
+function addAllAdjacent(
+  octopus: Octopus,
+  grid: number[][],
+  toVisit: Octopus[]
+) {
   const { row, col } = octopus
-  const up = row - 1 >= 0
-  const down = row + 1 < octopi.length
-  const right = col + 1 < octopi[octopi.length - 1].col
-  const left = col - 1 >= 0
 
-  const toVisit: Array<Octopus> = []
+  const up = row - 1
+  const down = row + 1
+  const left = col - 1
+  const right = col + 1
+  const hasUp = up >= 0
+  const hasDown = down < grid.length
+  const hasRight = right < grid[row].length
+  const hasLeft = left >= 0
 
-  if (up) {
-    // up
-    const o = findOctopusById(
-      JSON.stringify({ row: row - 1, col}),
-      octopi
-    )
-    if(o){
-      toVisit.push(o)
-    }
+  if (hasUp) {
+    const o = createOctopus(up, col)
+    toVisit.push(o)
   }
-  if (up && right) {
-    // up right
-    const o = findOctopusById(JSON.stringify({ row: row - 1, col: col + 1 }), octopi)
-    if (o) {
-      toVisit.push(o)
-    }
+  if (hasUp && hasRight) {
+    const o = createOctopus(up, right)
+    toVisit.push(o)
   }
-  if (up && left) {
-    // up left
-      const o = findOctopusById(
-        JSON.stringify({ row: row - 1, col: col - 1 }),
-        octopi
-      )
-      if (o) {
-        toVisit.push(o)
-      }
+  if (hasUp && hasLeft) {
+    const o = createOctopus(up, left)
+    toVisit.push(o)
   }
-  if (down) {
-    // down
-      const o = findOctopusById(
-        JSON.stringify({ row: row + 1, col }),
-        octopi
-      )
-      if (o) {
-        toVisit.push(o)
-      }
+  if (hasDown) {
+    const o = createOctopus(down, col)
+    toVisit.push(o)
   }
-  if (down && right) {
-    // down right
+  if (hasDown && hasRight) {
+    const o = createOctopus(down, right)
+    toVisit.push(o)
   }
-  if (down && left) {
-    // down left
+  if (hasDown && hasLeft) {
+    const o = createOctopus(down, left)
+    toVisit.push(o)
   }
-  if (left) {
-    // left
+  if (hasLeft) {
+    const o = createOctopus(row, left)
+    toVisit.push(o)
   }
-  if (right) {
-    // right
+  if (hasRight) {
+    const o = createOctopus(row, right)
+    toVisit.push(o)
   }
   return
 }
 
 function solve(grid: number[][]) {
-  const octopi = createOctopusMap(grid)
-  for (const step of [...new Array(100).fill(0)]) {
+  let flashes = 0
+  const gridTwo = JSON.parse(JSON.stringify(grid))
+  for (const _ of [...new Array(100).fill(0)]) {
+    flashes += octopusFlash(grid)
+    resetFlashed(grid)
+  }
+  console.log(flashes)
+  let step = 0;
+  flashes = 0
+  while(flashes < grid.length * grid[0].length){
+    step += 1;
+    flashes = octopusFlash(gridTwo)
+    resetFlashed(gridTwo)
+  }
+  console.log(step)
+}
 
-    console.log(step)
+function octopusFlash(grid: number[][]) {
+  let flashes = 0
+  const rows = grid.length
+  const cols = grid[0].length
+  for (const r of [...Array(rows).keys()]) {
+    for (const c of [...Array(cols).keys()]) {
+      const toVisit: Octopus[] = []
+      toVisit.push(createOctopus(r, c))
+      while (toVisit.length) {
+        const o = toVisit.shift()
+        if (o) {
+          const { row, col } = o
+          grid[row][col] += 1
+          if (grid[row][col] === 10) {
+            flashes += 1
+            addAllAdjacent(o, grid, toVisit)
+          }
+        }
+      }
+    }
+  }
+  return flashes
+}
+
+function resetFlashed(grid: number[][]) {
+  const rows = grid.length
+  const cols = grid[0].length
+  for (const r of [...Array(rows).keys()]) {
+    for (const c of [...Array(cols).keys()]) {
+      if (grid[r][c] > 9) {
+        grid[r][c] = 0
+      }
+    }
   }
 }
 type Octopus = {
   id: string
-  energy: number
   row: number
   col: number
-  hasFlashed: boolean
 }
-function createOctopus(row: number, col: number, energy: number = 0) {
+function createOctopus(row: number, col: number) {
   return {
     id: JSON.stringify({ row: row, col: col }),
-    energy,
     row,
     col,
-    hasFlashed: false,
   }
 }
 solve(grid)
