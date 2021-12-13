16afa2d83f6751e08ebd10efae82b5b6fa0ccda0
adam <adam@harpur.io>
2021-12-13 10:01:54 +0000

    settings

--- day-12/index.ts
+++ day-12/index.ts
@@ -1,10 +1,49 @@
 const input = Deno.readTextFileSync("input")
   .split("\n")
-  .map((x: string) => x.split("-"))
-// create map
-// populate map
-// part one
-function solve(input: string[]) {
-  console.log(input)
+  .map((x: string) => x.split("-"));
+
+const caves = new Map<string, string[]>();
+
+for (const [a, b] of input) {
+  if (a !== "start" && b !== "end") {
+    caves.set(b, (caves.get(b) || []).concat(a));
+  }
+  if (a !== "end" && b !== "start") {
+    caves.set(a, (caves.get(a) || []).concat(b));
+  }
 }
-solve(input)
+// part one
+function partOne(): number {
+  let count = 0;
+  const paths: [string[], boolean][] = [[["start"], true]];
+  const smallCaves: {
+    [node: string]: number;
+  } = {};
+  while (paths.length) {
+    // @ts-expect-error
+    const [prev, visited] = paths.pop();
+
+    const current = prev.at(-1);
+    const nodes = caves.get(current)!;
+    if (!prev || !current || !nodes) {
+      continue;
+    }
+    for (const node of nodes) {
+      if (node === "end") {
+        count += 1;
+      } else if (node.toUpperCase() === node || prev.includes(node)) {
+        paths.push([prev.concat(node), visited]);
+      } else if (visited) {
+        smallCaves[node] = 1;
+        smallCaves[node] += 1;
+        paths.push([prev.concat(node), false]);
+      }
+    }
+  }
+  return count;
+}
+
+function solve() {
+  partOne();
+}
+solve();
