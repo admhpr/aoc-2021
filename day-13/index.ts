525852ffa0043c16eeb700751850d58db8d33af3
adam <adam@harpur.io>
2021-12-15 10:10:32 +0000

    day 13

--- day-13/index.ts
+++ day-13/index.ts
@@ -1,11 +1,15 @@
 const input = Deno.readTextFileSync("input").split("\n");
 function parseInput(input: string[]) {
-  const grid: Dot[] = [];
   const instructions: [string, number][] = [];
+  const allX = [];
+  const allY = [];
+  const plot = [];
   for (const line of input) {
     if (line.length < 14 && line.length > 0) {
       const [x, y] = line.split(",").map((v) => Number(v));
-      grid.push(createDot(y, x));
+      allX.push(x);
+      allY.push(y);
+      plot.push([x, y]);
     } else if (
       line.length !== 0
     ) {
@@ -20,85 +24,38 @@ function parseInput(input: string[]) {
       );
     }
   }
+  const maxX = Math.max(...allX);
+  const maxY = Math.max(...allY);
+  const grid = new Array(maxY + 1).fill(".").map(() =>
+    new Array(maxX + 1).fill(".")
+  );
+  for (const [x, y] of plot) {
+    grid[y][x] = "#";
+  }
+
   return {
     grid,
     instructions,
   };
 }
+
+function removeRow(from: number, to: number, grid: string[][]) {
+  const folded = [...grid];
+  folded.splice(from, to + 1);
+  return folded;
+}
+function removeCol(from: number, to: number, grid: string[][]) {
+  const folded = [...grid];
+  folded.forEach((row) => row.splice(from, to + 1));
+  return folded;
+}
+
 function solve(input: string[]) {
   const { grid, instructions } = parseInput(input);
-  let foldedGrid = [...grid];
-  let firstFold = true;
-  for (const [foldOn, foldAlong] of instructions) {
-    for (const dot of foldedGrid) {
-      if (foldOn === "x") {
-        if (dot.col > foldAlong) {
-          const newX = foldAlong - (dot.col - foldAlong);
-          console.log(dot.col, newX, foldAlong);
-          if (
-            foldedGrid.find(({ id }) =>
-              id === JSON.stringify({ row: dot.row, col: newX })
-            )
-          ) {
-            foldedGrid = foldedGrid.filter(({ id }) =>
-              id !== JSON.stringify({ row: dot.row, col: newX })
-            );
-          } else {
-            dot.col = newX;
-            dot.id = JSON.stringify({ row: dot.row, col: newX });
-          }
-        }
-      } else if (foldOn === "y") {
-        if (dot.row > foldAlong) {
-          const newY = foldAlong - (dot.row - foldAlong);
-          if (
-            foldedGrid.find(({ id }) =>
-              id === JSON.stringify({ row: newY, col: dot.col })
-            )
-          ) {
-            foldedGrid = foldedGrid.filter(({ id }) =>
-              id !== JSON.stringify({ row: newY, col: dot.col })
-            );
-          } else {
-            dot.row = newY;
-            dot.id = JSON.stringify({ row: newY, col: dot.col });
-          }
-        }
-      }
-    }
-    if (firstFold) {
-      console.log(foldedGrid.length);
-      firstFold = false;
-    }
-  }
-  let maxRow = -1;
-  let maxCol = -1;
-  console.log(foldedGrid);
-  for (const { row, col } of foldedGrid) {
-    if (row > maxRow) {
-      maxRow = row;
-    }
-    if (col > maxCol) {
-      maxCol = col;
-    }
-  }
-  const g: string[][] = [];
-  for (const r of [...Array(maxRow).keys()]) {
-    if (!g[r]) {
-      g.push([]);
-    }
-    for (const c of [...Array(maxCol).keys()]) {
-      if (
-        foldedGrid.find(({ id }) => id === JSON.stringify({ row: r, col: c }))
-      ) {
-        g[r].push("#");
-      } else {
-        g[r].push(".");
-      }
-    }
-  }
-  console.log(g.map((row) => row.join("")).join("\r\n"));
+  console.log(grid);
+  const foldedGrid = [...grid];
 }
+
 solve(input);
 
 type Dot = {
